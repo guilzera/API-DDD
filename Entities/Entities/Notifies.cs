615ba398e466ad9a718ffe5d3463e43a63d705c5
Guilherme Nascimento <guiguidefarias@hotmail.com>
2022-07-28 11:51:17 -0300

    Modelo de entidades criada

---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+        public Notifies()
+        {
+            Notitycoes = new List<Notifies>();
+        }
+
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatorio",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidaPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatorio",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
