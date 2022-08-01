f494a91a6cc08381684f6cd80ea1d48eb1566b09
Guilherme Nascimento <guiguidefarias@hotmail.com>
2022-08-01 11:26:24 -0300

    teste

---
+++ Infrastructure/Repositories/Repositories/RepositoryMessage.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repositories.Generics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Repositories
+{
+    public class RepositoryMessage : RepositoryGeneric<Message>, IMessage
+    {
+        private readonly DbContextOptions<ContextBase> _context;
+
+        public RepositoryMessage()
+        {
+            _context = new DbContextOptions<ContextBase>();
+        }
+    }
+}
