19f41cc429aa6c0aad6b46d5edf69fc0335b5986
Guilherme Nascimento <guiguidefarias@hotmail.com>
2022-08-02 12:24:13 -0300

    Implementação do Token, Models e configuração do Program.cs

---
+++ WebAPIs/Program.cs
@@ -0,0 +1,107 @@
+using AutoMapper;
+using Domain.Interfaces;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Services;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repositories.Generics;
+using Infrastructure.Repositories.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIs.Models;
+using WebAPIs.Token;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ConfigServices
+builder.Services.AddDbContext<ContextBase>(options =>
+              options.UseSqlServer(
+                  builder.Configuration.GetConnectionString("DefaultConnextion")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Interface e Repositorio
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGeneric<>));
+builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+// serviço dominio
+builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "Teste.Security.Bearer",
+            ValidAudience = "Teste.Security.Bearer",
+            IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+        };
+
+        option.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = context =>
+            {
+                Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+// AutoMapper
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<MessageViewModel, Message>();
+    cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var devClient = "http://localhost:4200";
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader().WithOrigins(devClient));
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseSwaggerUI();
+
+app.Run();
